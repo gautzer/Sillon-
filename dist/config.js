c4c8d7e2fd035125687302902b50b30e77378697
gautzer <gautier.lucas09@hotmail.com>
2026-06-04 23:56:35 +0200

    Update Supabase configuration in config.js

--- dist/config.js
+++ dist/config.js
@@ -11,7 +11,7 @@
    ===================================================================== */
 
 window.SILLON_CONFIG = {
-  SUPABASE_URL: "COLLEZ_VOTRE_URL_ICI",
-  SUPABASE_ANON_KEY: "COLLEZ_VOTRE_CLE_ANON_ICI",
+  SUPABASE_URL: "https://azrqmafulstbiepcqytp.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6cnFtYWZ1bHN0YmllcGNxeXRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMjAxNDEsImV4cCI6MjA5NTg5NjE0MX0.sXwHU198GDqkkcPKJZnq3VM7xeEoI6Xh9wM7e8RcCP0",
   VAPID_PUBLIC_KEY: "COLLEZ_VOTRE_CLE_VAPID_PUBLIQUE_ICI",
 };
